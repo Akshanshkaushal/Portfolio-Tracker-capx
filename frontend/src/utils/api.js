97b0960e050102abb69ce603890070f866f335d9
Akshanshkaushal <akshanshkaushal9@gmail.com>
2024-12-26 17:25:20 +0530

    deployed backend api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://portfolio-tracker-backend-keer.onrender.com/api',
 });
 
 export default api;
